b840036bcea96d8c8d5962e84c92f6cc646f1b54
Gor <7495500@gmail.com>
2021-03-08 20:52:42 +0300

    Add geocode params

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,4 +1,11 @@
 class PublicController < ApplicationController
+
   def index
+    if Rails.env.production?
+      @country = request.location.country
+      @country = request.location.city
+      @currency = request.currency
+    end
   end
+  
 end
