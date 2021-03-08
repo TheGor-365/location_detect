ab09b9c0c90678d1caf96b019f73b6a210883129
Gor <7495500@gmail.com>
2021-03-08 23:37:18 +0300

    Refactoring

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,10 +2,11 @@ class PublicController < ApplicationController
 
   def index
     if Rails.env.production?
-      @country = request.location.country
+      @country = request.location.country_code
       @country = request.location.city
-      @currency = request.currency
+
+      @currency = @country.upcase == 'AU' ? 'AUD' : 'USD'
     end
   end
-  
+
 end
